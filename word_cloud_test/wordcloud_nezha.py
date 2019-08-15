28b81d69c57edc67bb6575c4ddaa89ebbdd7b45d
jayhebe <jayhebe1983@sina.com>
2019-08-15 11:19:42 +0800

    PDA exercises 20190815

--- word_cloud_test/wordcloud_nezha.py
+++ word_cloud_test/wordcloud_nezha.py
@@ -13,7 +13,9 @@ import sys
 def get_page_info(html_code):
     page_bs = BeautifulSoup(html_code, "html.parser")
     page_comments = page_bs.select("span[class='short']")
-    page_next = page_bs.select("a[class='next']")[0].get("href")
+    page_next = page_bs.select("a[class='next']")
+    if len(page_next) != 0:
+        page_next = page_next[0].get("href")
 
     return page_comments, page_next
 
@@ -32,8 +34,8 @@ def filter_text(comment):
 def get_cookies(path):
     f_cookies = open(path, 'r')
     cookies = {}
-    for line in f_cookies.read().split(';'):
-        name, value = line.strip().split('=', 1)
+    for item in f_cookies.read().split(';'):
+        name, value = item.strip().split('=', 1)
         cookies[name] = value
     return cookies
 
@@ -48,15 +50,39 @@ def get_cutted_comments(comments):
     return jieba.cut(comments, cut_all=False)
 
 
-def generate_wordcloud(image_path, text, target_file_path):
+def write_comments_to_file(movie_id, page_cookies, target_file_path):
+    base_url = "https://movie.douban.com/subject/" + movie_id + "/comments"
+    first_url = base_url + "?start=0&limit=20&sort=new_score&status=P"
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+        'Connection': 'keep-alive'
+    }
+
+    print("Processing: ", first_url)
+    html_code = requests.get(first_url, cookies=page_cookies, headers=header).content
+    page_comments, page_next = get_page_info(html_code)
+
+    while page_next:
+        with open(target_file_path, 'a', encoding='utf-8') as f_comments:
+            for page_comment in page_comments:
+                f_comments.writelines(filter_text(page_comment.get_text()))
+
+        new_url = base_url + page_next
+        print("Processing: ", new_url)
+        html_code = requests.get(new_url, cookies=page_cookies, headers=header).content
+        page_comments, page_next = get_page_info(html_code)
+
+
+def generate_wordcloud(bg_image_path, text, target_file_path):
     os_platform = sys.platform
     if os_platform.startswith("win"):
-        wc_font = r'C:\Windows\Fonts\FZSTK.TTF'
+        wc_font = r'C:\Windows\Fonts\simhei.ttf'
     elif os_platform.startswith("dar"):
         wc_font = "Hiragino Sans GB.ttc"
     else:
         wc_font = None
-    wc_background_image = imread(image_path)
+    wc_background_image = imread(bg_image_path)
     wc = WordCloud(collocations=False,
                    mask=wc_background_image,
                    font_path=wc_font,
@@ -74,31 +100,15 @@ def generate_wordcloud(image_path, text, target_file_path):
 
 
 if __name__ == '__main__':
-    page_cookies = get_cookies("cookies.txt")
-    # print(page_cookies)
-    base_url = "https://movie.douban.com/subject/26794435/comments"
-    first_url = "https://movie.douban.com/subject/26794435/comments?start=0&limit=20&sort=new_score&status=P"
-    header = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
-        'Connection': 'keep-alive'
-    }
+    douban_movie_id = "26931786"
+    douban_page_cookies = get_cookies("cookies.txt")
+    target_comments_file = "comments_spiderman.txt"
+    target_wordcloud_file = "word_cloud_spiderman.jpg"
+    word_cloud_bg_image = "spiderman.jpg"
 
-    print("Processing: ", first_url)
-    html_code_nezha = requests.get(first_url, cookies=page_cookies, headers=header).content
-    page_comments_nezha, page_next_nezha = get_page_info(html_code_nezha)
+    write_comments_to_file(douban_movie_id, douban_page_cookies, target_comments_file)
 
-    while page_next_nezha:
-        with open("comments_nezha.txt", 'a', encoding='utf-8') as f_nezha:
-            for page_comment in page_comments_nezha:
-                f_nezha.write(filter_text(page_comment.get_text()))
-
-        new_url = base_url + page_next_nezha
-        print("Processing: ", new_url)
-        html_code_nezha = requests.get(new_url, cookies=page_cookies, headers=header).content
-        page_comments_nezha, page_next_nezha = get_page_info(html_code_nezha)
-
-    with open("comments_nezha.txt", "rb") as f_result:
+    with open(target_comments_file, "rb") as f_result:
         all_comments = f_result.read()
 
-    generate_wordcloud("bj.png", " ".join(get_cutted_comments(all_comments)), "nezha_word_cloud.png")
+    generate_wordcloud(word_cloud_bg_image, " ".join(get_cutted_comments(all_comments)), target_wordcloud_file)
